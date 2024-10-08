c7c873b18fcd4132c5e1ec314164903b915825ef
Aleksandar Petrović <charlie.nis@gmail.com>
2024-10-09 01:56:37 +0200

    firebase, tailwindcss, routing, authentication boilplate done

---
+++ src/pages/App.tsx
@@ -0,0 +1,13 @@
+import { AuthForm } from "../components";
+
+const App = () => {
+  return (
+    <div className="grid place-items-center">
+      <div className="w-3/6 max-md:w-full mt-5">
+        <AuthForm />
+      </div>
+    </div>
+  );
+};
+
+export default App;
